6fe30673729ad958f9bca4d6a2c3c39d81c8dac5
Andrew Mykytiuk <andrew.mykytiuk@mati.io>
2022-06-20 20:21:30 +0200

    Add all

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:mati_plugin_flutter/mati_plugin_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mati flutter plugin demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void showMetaMapFlow() {
+    MetaMapFlutter.showMetaMapFlow("610b96fb7cc893001b135505", "610b96fb7cc893001b135504", {});
+    MetaMapFlutter.resultCompleter.future.then((result) => Fluttertoast.showToast(
+        msg: result is ResultSuccess ? "Success ${result.verificationId}" : "Cancelled",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("MetaMap flutter plugin demo"),
+        ),
+        body: Center(
+            child: ElevatedButton(
+              onPressed: showMetaMapFlow,
+              child: const Text('Verify me'),
+            )
+        )
+    );
+  }
+}
